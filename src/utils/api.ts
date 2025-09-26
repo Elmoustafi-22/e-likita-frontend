ab07a0e346a5f8d7158ff91015a17412b93bcccc
elmoustafi-22 <mustophaqadir@gmail.com>
2025-09-26 06:31:53 +0100

    remove console log from createPatient function in api utility

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,7 +4,6 @@ import axios from "axios";
 const API_URL = "https://e-likita-backend-eedl.onrender.com/api";
 
 export const createPatient = async (patientData: any) => {
-  console.log(patientData)
   const response = await axios.post(`${API_URL}/patients`, patientData);
   return response.data;
 };
